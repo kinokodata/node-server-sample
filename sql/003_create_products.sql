0bb8162cf9a76f1a1b0ae432c6f7023a029fd035
hyperkinoko <github.hyperkinoko@kinokodata.net>
2025-01-25 17:27:35 +0900

    テーブル構造、データの修正

--- sql/003_create_products.sql
+++ sql/003_create_products.sql
@@ -21,6 +21,12 @@ CREATE TABLE daily_sales (
     product_id INTEGER REFERENCES products(id),
     quantity INTEGER NOT NULL,
     total_amount INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 気象データテーブルの作成
+CREATE TABLE daily_weather (
+    date DATE PRIMARY KEY,
     weather_condition VARCHAR(20),
     max_temperature DECIMAL(4,1),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
