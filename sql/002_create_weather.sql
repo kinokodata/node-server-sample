4ec669ef290d299c563da0f93f50627c608ad319
hyperkinoko <github.hyperkinoko@kinokodata.net>
2025-01-26 15:45:58 +0900

    データベースの初期化sqlをコピペ用に1ファイルに纏める

--- sql/002_create_weather.sql
+++
@@ -1,7 +0,0 @@
--- 気象データテーブルの作成
-CREATE TABLE daily_weather (
-    date DATE PRIMARY KEY,
-    weather_condition VARCHAR(20),
-    max_temperature DECIMAL(4,1),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
